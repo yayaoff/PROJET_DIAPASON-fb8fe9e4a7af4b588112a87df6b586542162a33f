70d64e7f2b19c2cf5c55ac6c6c89699f31c9efe7
yaya.off <yasminescimmia@hotmail.it>
2023-05-19 16:45:30 +0200

    ib

--- headers/opti.h
+++ headers/opti.h
@@ -1,6 +1,6 @@
 #include "freq.h"
 
-#define TOL 1e-7
+#define TOL 5e-6
 #define f_target 1244.51    //frequence for D5# [Hz] 
 
 double *compute_freq(int tag_vis,param_t *param);
--- src/optimize.c
+++ src/optimize.c
@@ -7,13 +7,13 @@
 
 double f(param_t* param, double param_test){
     // Fonction dont on cherche la racine
-    double param_prev = param->l;
-    param->l = param_test;
+    double param_prev = param->e;
+    param->e = param_test;
     printf("Testing for param : %.9e\n",param_test);
     double *f_actuals = compute_band_freq(0,param);
     // double *f_actuals = compute_band_freq(0,param);
     printf("Freq act = %f\n",f_actuals[0]);
-    param->l = param_prev;
+    param->e = param_prev;
     return fabs(f_actuals[0] - f_target);
 }
 
@@ -31,41 +31,46 @@ double bissection_method(double low_bound, double up_bound,param_t *param){
     double mid = (low_bound + up_bound) / 2.0;
     
     double err = fabs(f(param,mid));
-    printf("--- > Initial error : %.9e\n",err);
-    int iter =0;
-    // while (fabs(f(param,mid)) > TOL && iter < 1000) {
-     while (fabs(up_bound - low_bound) > TOL && iter < 1000) {
-      if (f(param,mid) < TOL) {
-        double err = fabs(f(param,mid)-f_target);
-        printf("Freq computed ! Err final = %.9e\n!!",err);
+    // printf("--- > Initial error : %.9e\n",err);
+    while (fabs(up_bound - low_bound) >  TOL) {
+     // while (fabs(up_bound - low_bound) > TOL) {
+      double param_prev = param->e;
+      param->e = mid;
+      err = fabs(compute_band_freq(0,param)[0] - f_target) ;
+      if (err < TOL) {
+        // double err = fabs(f(param,mid)-f_target);
+        //printf("Freq computed ! Err final = %.9e\n!!",err);
         return mid;                     // Racine trouvée !
       }
-      else if (f(param,mid)*f(param,low_bound) < 0 ){
+      else{
+        param->e = param_prev;
+        if (f(param,mid)*f(param,low_bound) < 0 ){
         printf("Changing upper bound from %.9e to %.9e\n",up_bound,mid);
         up_bound = mid;
-      }  
-      else{
+        }  
+        else{
         printf("Changing lower bound from %.9e to %.9e\n",low_bound,mid);
         low_bound = mid;
+        }
       }
+
       mid = (up_bound + low_bound) / 2.0;
+      // err = fabs(f(param,mid));
       printf("OK\n");
-      // printf("Err act = %.9e\n",fabs(f(param,mid)));
       printf("low bound = %.9e\n",low_bound);
       printf("up bound = %.9e\n",up_bound);
-      iter++;
     }
     return mid;
 }
 
 double f_half(param_t* param, double param_test){
     // Fonction dont on cherche la racine
-    double param_prev = param->l;
-    param->l = param_test;
+    double param_prev = param->e;
+    param->e = param_test;
     // printf("Testing for param : %.9e\n",param_test);
     double *f_actuals = compute_band_freq_half(0,param);
     // printf("Freq act = %f\n",f_actuals[0]);
-    param->l = param_prev;
+    param->e = param_prev;
     return fabs(f_actuals[0] - f_target);
 }
 
@@ -83,29 +88,33 @@ double bissection_half_method(double low_bound, double up_bound,param_t *param){
     double mid = (low_bound + up_bound) / 2.0;
     
     double err = fabs(f_half(param,mid));
-    printf("--- > Initial error : %.9e\n",err);
-    int iter =0;
-    //while (err > TOL && iter < 1000) {
-     while (fabs(up_bound - low_bound) > TOL && iter < 1000) {
-      if (f_half(param,mid) < TOL) {
-        double err = fabs(f_half(param,mid)-f_target);
-        printf("Freq computed ! Err final = %.9e\n!!",err);
+    // printf("--- > Initial error : %.9e\n",err);
+    while (err > TOL) {
+     // while (fabs(up_bound - low_bound) > TOL) {
+      double param_prev = param->e;
+      param->e = mid;
+      if (fabs(compute_band_freq_half(0,param)[0] - f_target) < TOL) {
+        // double err = fabs(f(param,mid)-f_target);
+        //printf("Freq computed ! Err final = %.9e\n!!",err);
         return mid;                     // Racine trouvée !
       }
-      else if (f_half(param,mid)*f_half(param,low_bound) < 0 ){
+      else{
+        param->e = param_prev;
+        if (f_half(param,mid)*f_half(param,low_bound) < 0 ){
         printf("Changing upper bound from %.9e to %.9e\n",up_bound,mid);
         up_bound = mid;
-      }  
-      else{
+        }  
+        else{
         printf("Changing lower bound from %.9e to %.9e\n",low_bound,mid);
         low_bound = mid;
+        }
       }
+
       mid = (up_bound + low_bound) / 2.0;
+      err = fabs(f_half(param,mid));
       printf("OK\n");
-      // printf("Err act = %.9e\n",fabs(f(param,mid)));
       printf("low bound = %.9e\n",low_bound);
       printf("up bound = %.9e\n",up_bound);
-      iter++;
     }
     return mid;
 }
